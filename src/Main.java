6ce7a62bbcf3115daf66c1a4548328eb51b0d075
samue <mbasu@mail.uc.edu>
2024-09-20 12:10:19 -0400

    init commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,15 @@ public class Main
 {
     public static void main(String[] args)
     {
-        
+       double springCost = 140.00;
+       double summerCost = 500.00;
+       double winterCost = 250.00;
+       double fallCost = 200.00;
+
+       double totalYearlyCost = springCost + summerCost + winterCost + fallCost;
+
+        System.out.println("total maintenance cost: " + totalYearlyCost);
+
+
     }
 }
